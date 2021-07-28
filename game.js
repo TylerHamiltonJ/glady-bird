df494e541cc41f93c0b20e083c841b7e082b577e
Tyler Hamilton <tyler.hamilton@versa.agency>
2021-07-28 15:57:29 +1000

    Adding more detail to sydney bg

--- game.js
+++ game.js
@@ -8,7 +8,7 @@ const DEGREE = Math.PI / 180;
 
 // LOAD SPRITE IMAGE
 const sprite = new Image();
-sprite.src = "img/sprite_sheet_20.png";
+sprite.src = "img/sprite_22.png";
 
 // LOAD SOUNDS
 const SCORE_S = new Audio();
